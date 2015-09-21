38236e2144f55f32df44cc67eeff0647d2ee2e4e
Jader Martins <jadermcs94@gmail.com>
2015-09-21 10:09:10 -0300

    edit trace

--- scripts/trace.py
+++ scripts/trace.py
@@ -1,2 +1,12 @@
+'''
+/*Gera vetores para os k-points, para a analise da estrutura de banda.*/
+'''
+import sys
+
 def main(lattice, vector[x,y,z]):
-	return 
+	veclatt = []
+        print veclatt 
+
+
+if __name__ == "__main__":
+    main(argv[1],argv[2])
