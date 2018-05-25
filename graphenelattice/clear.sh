71e3b7949d25d3ceffbc99b6753d180a456fe245
Jader Martins <jadermcs94@gmail.com>
2018-05-24 21:10:44 -0300

    rm trash

--- graphenelattice/clear.sh
+++
@@ -1,8 +0,0 @@
-mkdir temp/
-cp *.psf temp/
-cp *.fdf temp/
-cp clear.sh temp/
-cp *.out temp/
-rm -f *
-cp temp/* .
-rm -r temp/
--- graphenerainer/clear.sh
+++
@@ -1,9 +0,0 @@
-mkdir temp/
-cp *.psf temp/
-cp *.fdf temp/
-cp clear.sh temp/
-cp a.out temp/
-cp *.x temp/
-rm -f *
-cp temp/* .
-rm -r temp/
